e851ade9a91c02497798e5b5b86a6e68e0e3749c
Veit <veitveit@gmail.com>
2018-06-27 11:07:09 +0200

    matrix processing

--- TestAPI/app.R
+++ TestAPI/app.R
@@ -17,7 +17,8 @@ Shiny.setInputValue("numrep", inmessage.numrep);
 
 }
 $(document).on("shiny:connected", function(event) {
-  var objects = [1, 2, 3, 4,2,1,2,3,3,4,3,2];  
+  var objects = ["a",1, 2, 3 ,4,"b", 4,2,1,2,"c", 
+3,3,4,2];  
 Shiny.setInputValue("data", objects);
 Shiny.setInputValue("numcond", 2);
 Shiny.setInputValue("numrep", 2);
@@ -34,17 +35,17 @@ Shiny.setInputValue("numrep", 2);
   server <- function(input, output, session) {
     dat <- NULL
     observe({
-      print(input$data)
+      print(input$numrep)
       if (!is.null(input$data)) {
       tdat <- matrix(as.vector(input$data),byrow = T, ncol=input$numrep*input$numcond+1)
-      print(dim(tdat))
-      dat <- matrix(as.numeric(tdat[,2:ncol(tdat)]),ncol=input$numrep*input$numcond, 
+      print(input$data)
+      dat <<- matrix(as.numeric(tdat[,2:ncol(tdat)]),ncol=input$numrep*input$numcond, 
                     dimnames = list(rows=tdat[,1], cols=paste(paste("C",rep(1:input$numcond, input$numrep),
-                                                                    "R", rep(1:input$numrep, each=input$numcond)))))
+                                                                    " R", rep(1:input$numrep, each=input$numcond),sep=""))))
       
 
             # dat <- unlist(lapply(dat,function(x) ifelse(length(x)>0,x,NULL)))
-      #print((dat)*2)
+      print(dat)
       
       
       # print(matrix(as.vector(dat),ncol=2,byrow=T))
